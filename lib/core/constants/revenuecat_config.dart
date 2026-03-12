a27203d76dfde8a662f423a7b5f2d54b1b884064
Marco Kang <k900327s@gmail.com>
2026-03-12 22:58:35 +0900

    설정: RevenueCat 의존성 및 설정 상수 추가
    - purchases_flutter ^9.14.0 추가 (^8.0.0은 freezed_annotation ^3.x와 충돌)
    - lib/core/constants/revenuecat_config.dart 생성 (iosApiKey, androidApiKey, entitlementId)
    - iOS pod install 완료 (RevenueCat 5.61.0, PurchasesHybridCommon 17.46.1)

---
+++ lib/core/constants/revenuecat_config.dart
@@ -0,0 +1,5 @@
+abstract final class RevenueCatConfig {
+  static const String iosApiKey = 'appl_REPLACE_WITH_IOS_KEY';
+  static const String androidApiKey = 'goog_REPLACE_WITH_ANDROID_KEY';
+  static const String entitlementId = 'premium';
+}
